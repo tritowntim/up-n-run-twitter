4ec927db513e8ee4ccd72d290e30eacaada3661d
Tim Dussinger <tritowntim@gmail.com>
2012-07-23 19:23:36 -0400

    examples 2-15, 2-16 is POST API and an assert test

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-var express = require('express')
+var express = require('express'),
+	port = 8000,
+	tweets = []
 
 var app = express.createServer()
+app.listen(port)
+console.log('listening on http://localhost:' + port + ' . . .')
 
 app.get('/', function(req, res) {
 
@@ -13,5 +17,16 @@ app.get('/yo', function(req, res) {
 	res.send('Don\'t get fresh yo')
 })
 
-app.listen(8000)
-console.log('listening on http://localhost:8000 . . .')
+app.post('/send', express.bodyParser(), function(req, res) {
+	if (req.body && req.body.tweet) {
+		tweets.push(req.body.tweet)
+		res.send({status: "ok", message: "so tweeted!"})
+	} else {
+		res.send({status: "no-go", message: "tweetless?"})
+	}
+})
+
+app.get('/tweets', function(req,res) {
+	res.send(tweets)
+})
+
---
+++ test.js
@@ -0,0 +1,32 @@
+var http = require('http'),
+	assert = require('assert')
+
+var opts = {
+	host: 'localhost',
+	port: 8000,
+	path: '/send',
+	method: 'POST',
+	headers: {'content-type':'application/x-www-form-urlencoded'}
+}
+
+var req = http.request(opts, function(res) {
+	res.setEncoding('utf8')
+
+	var data = ''
+	res.on('data', function(d) {
+		data += d
+	})
+
+	res.on('end', function() {
+		assert.strictEqual(data, '{"status":"ok","message":"so tweeted!"}')
+		console.log('test passed')
+	})
+
+})
+
+req.write('tweet=test')
+req.end()
+
+
+
+
